2a04fe48259b8ddc0b5522dc0232819fb3ab3c87
Kelsee Watts <123975076+chillgatez@users.noreply.github.com>
2023-05-20 10:58:02 -0500

    3 day forecast

--- city_explorer/src/Weather.js
+++ city_explorer/src/Weather.js
@@ -8,15 +8,8 @@ function Weather(props) {
     <> {props.forecastData.map((value) => {
       return (
         <div>
-          <h3>Forecast for {value[0].date}</h3>
-          <p>{value[0].description}</p>
-
-          <h3>Forecast for {value[1].date}</h3>
-          <p>{value[1].description}</p>
-
-
-          <h3>Forecast for {value[2].date}</h3>
-          <p>{value[2].description}</p>
+          <h3>Forecast for {value.date}</h3>
+          <p>{value.description}</p>
         </div>
 
       )
